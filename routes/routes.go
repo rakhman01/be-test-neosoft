7adeb0859ec995458b7997a85e655d2ce416deb2
Wakhid Hidayatur Rakhman <>
2025-10-27 07:31:02 +0700

    fix cross

--- routes/routes.go
+++ routes/routes.go
@@ -12,7 +12,7 @@ func SetupRouter(db *gorm.DB) *gin.Engine {
 
     //handle cors
         r.Use(cors.New(cors.Config{
-        AllowOrigins: []string{"http://localhost:5173"},
+        AllowOrigins: []string{"http://localhost:5173",  "https://your-vercel-app.vercel.app"},
         AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
         AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
     }))
